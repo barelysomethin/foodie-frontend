ab8c0c90250d5b2d626948bcb01e4c04ef6fdfd7
barelysomethin <132691959+barelysomethin@users.noreply.github.com>
2024-07-15 11:23:33 -0700

    added more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Foodcard from './components/Foodcard';
+import Greeting from './components/Greeting';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -8,7 +10,9 @@ function App() {
   return (
     <>
     <div className='container'>
+    <Greeting/>
     <Foodcard />
+    <Footer/>
     </div>
      
     </>
