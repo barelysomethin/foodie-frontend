bdce3e8f0e431c0c5f6f5705cfb5e7fd8552e280
barelysomethin <132691959+barelysomethin@users.noreply.github.com>
2024-07-15 11:44:35 -0700

    added more code

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Greeting = () => {
-  return (
-    <div>Greeting</div>
-  )
+    return (
+        <>
+            <div class="greeting">
+                <p>Good morning</p>
+                <h1>Prakhar</h1>
+                <img src="./prakharimage.jpg" alt="User Image" class="user-image" />
+            </div>
+        </>
+    )
 }
 
 export default Greeting
